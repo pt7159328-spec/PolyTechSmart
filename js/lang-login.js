30fff476fb6c3cb358218c7a9d405b24b1b77b78
Prakash Tiwari <pt7159328@gmail.com>
2026-02-03 09:25:30 +0530

    Add files via upload

---
+++ js/lang-login.js
@@ -0,0 +1,29 @@
+const translations = {
+    en: {
+        login_title: "Welcome Back",
+        login_desc: "Login to continue your learning journey",
+
+        ph_email: "Enter your email",
+        ph_password: "Enter your password",
+
+        forgot_password: "Forgot password?",
+        login_btn: "Login",
+
+        new_user: "New user?",
+        register_here: "Register here"
+    },
+
+    hi: {
+        login_title: "वापस स्वागत है",
+        login_desc: "अपनी पढ़ाई जारी रखने के लिए लॉगिन करें",
+
+        ph_email: "अपना ईमेल लिखें",
+        ph_password: "अपना पासवर्ड लिखें",
+
+        forgot_password: "पासवर्ड भूल गए?",
+        login_btn: "लॉगिन करें",
+
+        new_user: "नए उपयोगकर्ता?",
+        register_here: "यहाँ रजिस्टर करें"
+    }
+};
